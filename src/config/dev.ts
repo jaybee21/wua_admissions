7a69bf17a423265833729bc77b602bf226d741b1
Jabule Simanga <hillarysimanga@gmail.com>
2025-01-08 15:00:47 +0200

    change of databases

--- src/config/dev.ts
+++ src/config/dev.ts
@@ -5,9 +5,9 @@ export const config: Config = {
   ...commonConfig,
   environment: 'development',
   database: {
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
+    host: process.env.DB_HOST || '10.0.0.43',
+    user: process.env.DB_USER || 'wua_user',
+    password: process.env.DB_PASSWORD || '#pass123',
     database: process.env.DB_NAME || 'wua_job_board_dev',
     port: parseInt(process.env.DB_PORT || '3306', 10),
   },
