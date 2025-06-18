63614d964a4416b310611a145b4eb75c005fa833
Jabule Simanga <hillarysimanga@gmail.com>
2025-06-18 09:05:46 +0200

    lets push this and see

--- src/config/dev.ts
+++ src/config/dev.ts
@@ -7,7 +7,7 @@ export const config: Config = {
   database: {
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
+    password: process.env.DB_PASSWORD || '"@99!4@dm!n',
     database: process.env.DB_NAME || 'wua_admissions',
     port: parseInt(process.env.DB_PORT || '3306', 10),
   },
