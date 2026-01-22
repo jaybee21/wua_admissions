82b9cf9fc440682a381168e1d9cb959f5660d36b
Jabule Simanga <hillarysimanga@gmail.com>
2026-01-22 10:40:09 +0200

    new ignored

--- src/db.ts
+++ src/db.ts
@@ -10,7 +10,7 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  connectTimeout: 10000,
+  connectTimeout: 1000000,
 });
 
 pool.getConnection()
